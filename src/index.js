520ee85a9f5f4d8455656b8fcafa1ae10c308316
CasLinden <cas_linden@msn.com>
2022-10-11 19:24:20 +0900

    seeing if pages works

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import './style.css';
 import printMe from './new.js';
 
 printMe()
+
+let lichaam = document.querySelector('body');
+
+let pee = document.createElement('p')
+pee.textContent = 'Dit is rood';
+lichaam.appendChild(pee);
